13196aa44243ba07ffdd63d25b06f101d82b9074
sandmanman <sandmanman@qq.com>
2017-02-13 16:20:01 +0800

    cooking first

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+    <div>
+        <h1>Hellow, {{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: 'VueJS World'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        position: relative;
+        padding: 30px;
+
+        color: #4499ee;
+        text-align: center;
+
+        &:after {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            height: 1px;
+            background-color: #ddd;
+        }
+    }
+</style>
