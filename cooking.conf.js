6fe933cc6069540f1721442ffd4b6ce252be4ab5
sandmanman <sandmanman@qq.com>
2017-02-14 19:36:32 +0800

    ...

--- cooking.conf.js
+++ cooking.conf.js
@@ -10,14 +10,14 @@ cooking.set({
         app: './src/index.js',  // 指定入口文件
     },
     dist: './dist', // 设置打包后的文件目录
-    //clear: true, // 每次打包都清理掉 dist 目录
+    clear: true, // 每次打包都清理掉 dist 目录
     hash: true, // 打包的文件是否带 hash
     sourceMap: true, // 是否带 sourceMap
     template: './index.html', // 加载 index.html 模板
     postcss: [
         require('postcss-salad')
     ],
-    extractCSS: true, // 提取 CSS 文件
+    extractCSS: 'app.css', // 提取 CSS 文件
     chunk: [
         'vendor', // entry 里定义的入口文件，也就是会将 vue 单独打包
         'manifest' // 这个并没有在 entry 里声明的会将所有公用部分打包，也就是 webpack runtime
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import routeConfig from './router-config'; // 引入router-config.js文件
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './app';
 
 //加载路由中间件
