754a0a088d9490630c351abb107606527a14b800
sandmanman <sandmanman@qq.com>
2017-04-10 14:43:37 +0800

    button组件，自定义图标/文字/事件

---
+++ src/components/button/vvButton.vue
@@ -0,0 +1,107 @@
+<template>
+    <button class="vv-btn" v-on:click="buttonClickEvent">
+        <slot name="icon"></slot>
+        <span>{{msg}}</span>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'vv-btn',
+        props: { // props 暴露组件属性接口
+            msg: {
+                default: 'button'
+            }
+        },
+        methods: {
+            buttonClickEvent() {
+                //alert("先弹出默认的文案");
+                this.$emit('buttonClickEvent'); 
+                //关键代码父组件触发自定义事件
+                //$emit，也叫触发机制，父组件监听，子组件触发
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    .vv-btn {
+        display: inline-block;
+        font-weight: normal;
+        line-height: 1.25;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        border: 1px solid transparent;
+        padding: 0.5rem 1rem;
+        font-size: 1rem;
+        border-radius: 0.25rem;
+        -webkit-transition: all 0.2s ease-in-out;
+        -o-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+    }
+
+    .vv-btn:focus,
+    .vv-btn:hover {
+        text-decoration: none;
+    }
+
+    .vv-btn:focus,
+    .vv-btn.focus {
+        outline: 0;
+        -webkit-box-shadow: 0 0 0 2px rgba(2, 117, 216, 0.25);
+        box-shadow: 0 0 0 2px rgba(2, 117, 216, 0.25);
+    }
+
+    .vv-btn.disabled,
+    .vv-btn:disabled {
+        cursor: not-allowed;
+        opacity: .65;
+    }
+
+    .vv-btn:active,
+    .vv-btn.active {
+        background-image: none;
+    }
+
+    a.vv-btn.disabled {
+        pointer-events: none;
+    }
+
+    .vv-btn {
+        color: #292b2c;
+        background-color: #fff;
+        border-color: #ccc;
+    }
+
+    .vv-btn:hover {
+        color: #292b2c;
+        background-color: #e6e6e6;
+        border-color: #adadad;
+    }
+
+    .vv-btn:focus,
+    .vv-btn.focus {
+        -webkit-box-shadow: 0 0 0 2px rgba(204, 204, 204, 0.5);
+        box-shadow: 0 0 0 2px rgba(204, 204, 204, 0.5);
+    }
+
+    .vv-btn.disabled,
+    .vv-btn:disabled {
+        background-color: #fff;
+        border-color: #ccc;
+    }
+
+    .vv-btn:active,
+    .vv-btn.active,
+    .show>.vv-btn.dropdown-toggle {
+        color: #292b2c;
+        background-color: #e6e6e6;
+        background-image: none;
+        border-color: #adadad;
+    }
+</style>
--- src/pages/button/button.vue
+++ src/pages/button/button.vue
@@ -1,98 +1,32 @@
 <template>
-    <button class="vv-btn">
-        <span>{{msg}}</span>
-    </button>
+    <div>
+        <vv-button msg="Google" v-on:buttonClickEvent="doSomething1">
+            <i slot="icon" class="fa fa-google"></i>
+        </vv-button>
+
+        <vv-button msg="Facebook" v-on:buttonClickEvent="doSomething2">
+            <i slot="icon" class="fa fa-facebook"></i>
+        </vv-button>
+    </div>
 </template>
 
 <script>
+    import vvButton from '../../components/button/vvButton';
     export default {
-        name: 'vv-btn',
-        data() {
-            return {
-                msg: 'button'
+        components: {
+            vvButton
+        },
+        methods: {
+            doSomething1() {
+                alert('自定义事件111')
+            },
+            doSomething2() {
+                alert('自定义事件222')
             }
         }
     }
 </script>
 
 <style lang="less">
-    .vv-btn {
-        display: inline-block;
-        font-weight: normal;
-        line-height: 1.25;
-        text-align: center;
-        white-space: nowrap;
-        vertical-align: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        border: 1px solid transparent;
-        padding: 0.5rem 1rem;
-        font-size: 1rem;
-        border-radius: 0.25rem;
-        -webkit-transition: all 0.2s ease-in-out;
-        -o-transition: all 0.2s ease-in-out;
-        transition: all 0.2s ease-in-out;
-    }
-
-    .vv-btn:focus,
-    .vv-btn:hover {
-        text-decoration: none;
-    }
-
-    .vv-btn:focus,
-    .vv-btn.focus {
-        outline: 0;
-        -webkit-box-shadow: 0 0 0 2px rgba(2, 117, 216, 0.25);
-        box-shadow: 0 0 0 2px rgba(2, 117, 216, 0.25);
-    }
-
-    .vv-btn.disabled,
-    .vv-btn:disabled {
-        cursor: not-allowed;
-        opacity: .65;
-    }
-
-    .vv-btn:active,
-    .vv-btn.active {
-        background-image: none;
-    }
-
-    a.vv-btn.disabled {
-        pointer-events: none;
-    }
-
-    .vv-btn {
-        color: #292b2c;
-        background-color: #fff;
-        border-color: #ccc;
-    }
-
-    .vv-btn:hover {
-        color: #292b2c;
-        background-color: #e6e6e6;
-        border-color: #adadad;
-    }
-
-    .vv-btn:focus,
-    .vv-btn.focus {
-        -webkit-box-shadow: 0 0 0 2px rgba(204, 204, 204, 0.5);
-        box-shadow: 0 0 0 2px rgba(204, 204, 204, 0.5);
-    }
-
-    .vv-btn.disabled,
-    .vv-btn:disabled {
-        background-color: #fff;
-        border-color: #ccc;
-    }
-
-    .vv-btn:active,
-    .vv-btn.active,
-    .show>.vv-btn.dropdown-toggle {
-        color: #292b2c;
-        background-color: #e6e6e6;
-        background-image: none;
-        border-color: #adadad;
-    }
+    
 </style>
